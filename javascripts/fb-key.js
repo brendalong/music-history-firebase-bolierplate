839c079525821402b447d1c031884e8f506f5648
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-03-01 14:52:39 -0600

    brenda code along

--- javascripts/fb-key.js
+++ javascripts/fb-key.js
@@ -2,10 +2,11 @@
 
 function getKey() {
   return {
-    apiKey: "reallyLongValue",
-    authDomain: "something.firebaseapp.com",
-    databaseURL: "https://something.firebaseio.com"
+    apiKey: "AIzaSyAd8WDmer3tC51l_o_D8iGtz5YJ5kItw5s",
+    authDomain: "my-awesome-project-3a53e.firebaseapp.com",
+    databaseURL: "https://my-awesome-project-3a53e.firebaseio.com"
   };
 }
 
 module.exports = getKey;
+
